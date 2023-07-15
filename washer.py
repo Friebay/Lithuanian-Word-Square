c1141ae7c769e510600f25e29c17b8568cd95044
Friebay <frybaylt@gmail.com>
2023-07-15 10:54:19 +0300

    moving forward

--- washer.py
+++ washer.py
@@ -1,23 +1,25 @@
 filemame = "1"
 
-# Save to file "w" = read mode
+# Save to file "w" = write mode
 def save_words_in_column(filename, words):
     with open(filename, "w", encoding="utf-8") as file:
         for word in words:
             file.write(word + "\n")
             
             
-def count_words(filename):
-    with open(filename, "r", encoding="utf-8") as file:
-        words = file.read().split()
-        return len(words)
-    
+def count_words(words):
+    return len(words)
 
  #Open the file "r" = read mode
 with open(filemame + ".txt", "r", encoding="utf-8") as file:
-    
-    # Read the contents of the file
-    words = file.read().split()
+    lines = file.readlines()
+    words = []
+    for line in lines:
+        line_words = line.strip().split()
+        if len(line_words) == 2:
+            words.extend(line_words)
+        else:
+            words.append(line.strip())
 
 unique_words = set()
 
@@ -37,10 +39,10 @@ for word in words:
  # Call the function to save words in a column format
 save_words_in_column(filemame + "_cleaned.txt", non_repeating_words)
 
+print(repeating_words[:10])
     
-    
-count = count_words(filemame + ".txt")
-cleaned_count = count_words(filemame + "_cleaned.txt")
+count = count_words(words)
+cleaned_count = count_words(non_repeating_words)
 
 print("Number of words in " + filemame + ".txt:", count)
 print("Number of words in " + filemame + "_cleaned.txt:", cleaned_count)
