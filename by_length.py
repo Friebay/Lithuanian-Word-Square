caf81b19ecf7499b24564f10e7ca4bb5b5b92197
Friebay <frybaylt@gmail.com>
2023-07-16 14:15:38 +0300

    remove

---
+++ by_length.py
@@ -0,0 +1,11 @@
+# Open the input file
+with open("wiki_lithuanianletters_no_special.txt", "r", encoding="utf-8") as input_file:
+    # Read the words from the input file
+    words = input_file.read().split()
+ # Sort the words by length
+sorted_words = sorted(words, key=len)
+ # Open the output file
+with open("length.txt", "w", encoding="utf-8") as output_file:
+    # Write the sorted words to the output file
+    for word in sorted_words:
+        output_file.write(word + "\n")
--- special_remover.py
+++ special_remover.py
@@ -7,7 +7,7 @@ filtered_lines = []
 # Iterate over each line
 for line in lines:
     # Check if the line contains any special characters, 'w', 'x', 'À', 'Ò', 'Í', or letters with a grave accent on the opposite side
-    if not any(char in line for char in ['.', ',', '-', "'", '`', '*', 'ʼ']) and 'Đ' not in line and 'Ý' not in line and 'Á' not in line and 'Ā' not in line and 'Ě' not in line and 'Ĩ' not in line and 'È' not in line and 'Ļ' not in line and 'Ö' not in line and 'Ú' not in line and 'W' not in line and 'X' not in line and 'À' not in line and 'Ò' not in line and 'Í' not in line and 'À'[::-1] not in line and 'Ò'[::-1] not in line and 'Í'[::-1] not in line:
+    if not any(char in line for char in ['.', ',', '-', "'", '`', '*', 'ʼ','·']) and 'Ù' not in line and 'Æ' not in line and 'Ç' not in line and 'Ê' not in line and 'Ć' not in line and 'Ş' not in line and 'Ī' not in line and 'Û' not in line and 'Ế' not in line and 'É' not in line and 'Ó' not in line and 'Q' not in line and 'Â' not in line and 'Ạ' not in line and 'Ä' not in line and 'Ü' not in line and 'Đ' not in line and 'Ý' not in line and 'Á' not in line and 'Ā' not in line and 'Ě' not in line and 'Ĩ' not in line and 'È' not in line and 'Ļ' not in line and 'Ö' not in line and 'Ú' not in line and 'W' not in line and 'X' not in line and 'À' not in line and 'Ò' not in line and 'Í' not in line and 'À'[::-1] not in line and 'Ò'[::-1] not in line and 'Í'[::-1] not in line:
         # If no special characters, add the line to the filtered list
         filtered_lines.append(line)
 # Open the file in write mode and overwrite its contents with the filtered lines
