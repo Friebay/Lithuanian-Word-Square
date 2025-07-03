ccaa264545f6945a0fb2641ff732cd31190fc861
Friebay <frybaylt@gmail.com>
2025-07-03 13:26:35 +0300

    update the words database

--- column_remover.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('lt-LT.dic.txt', 'r', encoding='utf-8') as file:
-    data = file.read()
- # Remove slashes and numbers while preserving Lithuanian letters
-clean_data = re.sub(r'[^a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ\s]', '', data)
-with open('words2.txt', 'w', encoding='utf-8') as file:
-    file.write(clean_data)
-print("Cleaned data has been saved in words2.txt.")
